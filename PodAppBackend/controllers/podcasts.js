cc88e89bb39724df4c60143730f0ddba60cc2d18
Monica Kosciuk <koscim@gmail.com>
2017-10-19 16:00:15 -0400

    Fetch from external api from within router.js Express

--- PodAppBackend/controllers/podcasts.js
+++ PodAppBackend/controllers/podcasts.js
@@ -1,12 +1,44 @@
 import Podcast from '../models/podcast';
 import moment from 'moment';
+import http from 'http';
+import https from 'https';
+import request from 'request';
 
 export const index = (req, res, next) => {
   // Find all podcasts and return json response
-  Podcast.find().lean().exec((err, podcasts) => res.json(
-    // Iterate through each podcast
-    { podcasts: podcasts.map(podcast => ({
-      ...podcast
-    }))}
-  ));
+  // Podcast.find().lean().exec((err, podcasts) => res.json(
+  //   // Iterate through each podcast
+  //   { podcasts: podcasts.map(podcast => ({
+  //     ...podcast
+  //   }))}
+  // ));
+  let options = {
+    host: 'itunes.apple.com',
+    port: 80,
+    path: '/search?term=podcast&genreId=1318&limit=50'
+  };
+
+  http.get(options, function(response) {
+    console.log("Got response: " + response.statusCode);
+    console.log('HEADERS: ' + JSON.stringify(response.headers));
+    response.setEncoding('utf8');
+    response.on('data', function(chunk) {
+      console.log('BODY: ' + chunk);
+    })
+  }).on('error', function(e) {
+    console.log("Got error: " + e.message);
+  });
+  console.log('YAYAYAYAYAY');
+  // http.get({
+  //   uri: 'https://itunes.apple.com/search?term=podcast&genreId=1318&limit=1000'
+  // }).pipe(res);
+  // console.log(res);
 };
+
+export const categories = (req, res, next) => {
+  // Search Itunes API for podcasts of a certain category and return json response
+  request({
+    uri: 'https://itunes.apple.com/search?term=podcast&genreId=1318&limit=1000'
+  }).pipe(res);
+  console.log(res);
+}
--- PodAppBackend/router.js
+++ PodAppBackend/router.js
@@ -1,12 +1,14 @@
 import express, { Router } from 'express';
-// Import index action from movies controller
+// Import index action from podcasts controller
 import { index } from './controllers/podcasts';
-
 // Initialize the router
 const router = Router();
 
-// Handle /podcasts.json route with index action from movies controller
+// Handle /podcasts.json route with index action from podcasts controller
 router.route('/podcasts.json')
   .get(index);
+
+// router.route('/categories.json')
+//   .get(categories);
 
 export default router;
